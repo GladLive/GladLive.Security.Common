a64ddebd6dee713dd0a2dcafa7c2d6f4ab069445
Andrew Blakely <andrew.blakely@ymail.com>
2016-03-21 11:21:56 -0500

    Added hashing interface

---
+++ src/GladLive.Security.Common/Hashing/IHashingService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GladLive.Security.Common
+{
+	/// <summary>
+	/// Service contract for hashing.
+	/// </summary>
+	public interface IHashingService
+	{
+		/// <summary>
+		/// Converts <paramref name="valueToHash"/> to the equivalent hash.
+		/// </summary>
+		/// <param name="valueToHash">String to hash.</param>
+		/// <returns>Hashed form of <paramref name="valueToHash"/>.</returns>
+		string Hash(string valueToHash);
+
+		/// <summary>
+		/// Compares two hashed values for underlying data equivalence.
+		/// </summary>
+		/// <param name="hashOne">Hash to compare to.</param>
+		/// <param name="hashTwo">Hash to compare.</param>
+		/// <returns>True if the underlying data is equal or the hash values are equal; false if otherwise.</returns>
+		bool isHashValuesEqual(string hashOne, string hashTwo);
+	}
+}
