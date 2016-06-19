acb0baeebe1bfc97fe060a81075184c7efdcbbeb
Andrew Blakely <andrew.blakely@ymail.com>
2016-06-19 02:34:20 -0500

    Create CertToRSA converter

---
+++ src/GladLive.Security.Common/RSA/CertToRSAProviderConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+
+namespace GladLive.Security.Common
+{
+	/// <summary>
+	/// Class converts a <see cref="X509Certificate2"/> into an <see cref="RSACryptoProvider"/> 
+	/// </summary>
+	public class CertToRSAProviderConverter
+	{
+		/// <summary>
+		/// RSA provider that was built using the provided <see cref="X509Certificate2"/>.
+		/// </summary>
+		public RSACryptoServiceProvider Provider { get; }
+
+		/// <summary>
+		/// Creates a <see cref="X509Certificate2"/> converter that produces an initialized instance of
+		/// <see cref="RSACryptoServiceProvider"/>.
+		/// </summary>
+		/// <param name="cert">Cert to use information from.</param>
+		/// <param name="includePrivateKey">Indicates if the private key should be included.</param>
+		public CertToRSAProviderConverter(X509Certificate2 cert, bool includePrivateKey)
+		{
+#if DNXCORE50 || DNX46
+			Provider.ImportParameters(cert.GetRSAPublicKey().ExportParameters(includePrivateKey));
+#endif
+
+#if DNX451 || NET45 || NET451
+			Provider.FromXmlString(cert.PrivateKey.ToXmlString(false));
+#endif
+		}
+	}
+}
